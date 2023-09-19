06a5c5a697c8e6a194736559872780cd7a7d0f20
JuaniBoiko <juaniboiko@gmail.com>
2023-09-19 16:07:36 -0300

    aaa

--- public/main.js
+++ public/main.js
@@ -17,6 +17,11 @@ setTimeout(function () {
     document.getElementsByTagName("html")[0].style.overflowY = "auto";
 }, 1700);
 
+window.onload = function() {
+    var video = document.getElementById('myVideo');
+    video.play();
+  };
+
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const sr = ScrollReveal({
     distance: '30px',
