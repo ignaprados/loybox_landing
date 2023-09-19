1b692a91a8fec2819f51f522104784efb883c27c
JuaniBoiko <juaniboiko@gmail.com>
2023-09-19 15:56:52 -0300

    aaa

--- public/main.js
+++ public/main.js
@@ -18,6 +18,14 @@ setTimeout(function () {
 }, 1700);
 
 
+var videoElement = document.querySelector('.preloaderVideo');
+
+// Agrega un evento touchstart al elemento de video para prevenir la acción táctil
+videoElement.addEventListener('touchstart', function (e) {
+    e.preventDefault();
+});
+
+
 /* ===== SCROLL REVEAL ANIMATION ===== */
 const sr = ScrollReveal({
     distance: '30px',
